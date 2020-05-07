f5d25c0e95450475ad96ab3c342bf2df555408a5
FoxHermit <62595041+FoxHermit@users.noreply.github.com>
2020-05-07 13:51:02 +0200

    Fini

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+$(function(){
+  //$('li').css('color', 'red');
+  $('ol').children().css('color', 'red'); // La fonction "children()" permet de cibler les enfants d'une balise (ici les li qui sont enfants de ol)
+});
